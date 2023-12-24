83f709357ebf39525dfb3fbaa01e0b5fcf468cf4
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-12-23 18:56:22 -0600

    useLayoutEffect

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,38 @@
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from '../components';
+
+const Layout = () => {
+
+    const { counter, increment } = useCounter(1);
+    const { data, hasError, isLoading } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
+
+    return (
+        <>
+            <h1>Breaking Bad Quotes</h1>
+            <hr />
+
+            {
+                isLoading
+                    ? <LoadingQuote />
+                    : (
+
+                        data.map((element, index) => {
+                            const { quote, author } = element;
+                            return <Quote quote={ quote } author={ author } key={ index }/>
+                        })
+                    )
+            }
+
+            <button 
+                className='btn btn-primary' 
+                disabled={ isLoading }
+                onClick={ () => increment() }
+            >
+                Load new quotes
+            </button>
+
+        </>
+    );
+};
+
+export default Layout;
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,9 +1,30 @@
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export const Quote = ({ quote, author }) => {
+
+    const pRef = useRef();
+
+    const [ boxSize, setBoxSize ] = useState({
+        width: 0, 
+        height: 0
+    });
+
+    useLayoutEffect(() => {
+        const { height, width } = pRef.current.getBoundingClientRect();
+        setBoxSize({ height, width });
+    }, []);
+
     return (
-        <blockquote className='blockquote text-end mb-6'>
-            <p className='mb-3'>{ quote }</p>
-            <footer className='blockquote-footer'>{ author }</footer>
-        </blockquote>
+        <>        
+            <blockquote 
+                className='blockquote text-end mb-6'
+                style={{ display: 'flex' }}
+            >
+                <p ref={ pRef } className='mb-3'>{ quote }</p>
+                <footer className='blockquote-footer'>{ author }</footer>
+            </blockquote>
+
+            <code style={{ display: 'block' }} className='mb-4'>{ JSON.stringify(boxSize) }</code>
+        </>
     );
 };
